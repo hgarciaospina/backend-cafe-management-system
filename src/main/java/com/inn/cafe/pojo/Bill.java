9a132fd25b4f2096da76d5057406949521972788
Henry García <henrygarciaospina@gmail.com>
2025-02-02 17:42:11 -0500

    API of Bill, Dashboard

--- src/main/java/com/inn/cafe/pojo/Bill.java
+++ src/main/java/com/inn/cafe/pojo/Bill.java
@@ -1,17 +1,16 @@
 package com.inn.cafe.pojo;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import java.io.Serial;
 import java.io.Serializable;
+
+@NamedQuery(name = "Bill.getAllBills", query = "select b from Bill b order by b.id desc")
+
+@NamedQuery(name = "Bill.getBillByUserName", query = "select b from Bill b where b.createdBy=:username order by b.id desc")
 @Data
 @Entity
 @DynamicUpdate
