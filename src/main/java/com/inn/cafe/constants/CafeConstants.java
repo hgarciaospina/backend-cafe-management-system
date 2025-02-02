be8dd738e0eb5469208db2cda2134bf1331ec2b2
Henry García <henrygarciaospina@gmail.com>
2025-02-01 23:19:30 -0500

    Generate Report API of Bill

--- src/main/java/com/inn/cafe/constants/CafeConstants.java
+++ src/main/java/com/inn/cafe/constants/CafeConstants.java
@@ -5,7 +5,8 @@ public class CafeConstants {
     CafeConstants(){
 
     }
-    public static final String UNAUTHORIZED_ACCESS = "Unauthorized access";
-    public static final String INVALID_DATA = "Invalid data";
-    public static final String SOMETHING_WENT_WRONG = "Something went wrong";
+    public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
+    public static final String INVALID_DATA = "Invalid data.";
+    public static final String SOMETHING_WENT_WRONG = "Something went wrong.";
+    public static final String STORE_LOCATION = "D:\\BTech Days Projects\\storedFiles\\cafe-stored-files";
 }
--- src/main/java/com/inn/cafe/utils/CafeUtils.java
+++ src/main/java/com/inn/cafe/utils/CafeUtils.java
@@ -1,8 +1,16 @@
 package com.inn.cafe.utils;
 
+import com.google.common.base.Strings;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class CafeUtils {
@@ -26,4 +34,20 @@ public class CafeUtils {
         return password.toString();
     }
 
+    public static String getUUID() {
+        Date date = new Date();
+        long time = date.getTime();
+        return "BILL-" + time;
+    }
+
+    public static JSONArray getJsonArrayFromString(String data) throws JSONException {
+        return new JSONArray(data);
+    }
+
+    public static Map<String, Object> getMapFromJson(String data) {
+        if (!Strings.isNullOrEmpty(data))
+            return new Gson().fromJson(data, new TypeToken<Map<String, Object>>() {
+            }.getType());
+        return new HashMap<>();
+    }
 }
